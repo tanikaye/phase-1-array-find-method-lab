4600f078abe43e02f92e7d02663dcffa8f10f4a0
Tani Kaye <1tanikaye@gmail.com>
2021-10-14 10:28:17 -0400

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  //...
+]
+
+function cb(element, index) {
+  console.log('logged ',element)
+return (element.result === "W")
+}
+
+function superbowlWin(record) {
+  if (record.find(cb) === undefined) {
+    return undefined;
+  }
+return record.find(cb).year;
+}
+
+console.log(superbowlWin(record))
+console.log('yoyo ', record.find(cb))
+
+
+
 
